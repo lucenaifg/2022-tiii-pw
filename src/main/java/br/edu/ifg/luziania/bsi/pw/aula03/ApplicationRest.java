f0375bb6b931e96ef9f23ded8f981551ac9f7d6c
Daniel Lucena <cto@mindify.net>
2022-07-13 09:20:13 -0300

    Introdução ao MVC com Quarkus

--- src/main/java/br/edu/ifg/luziania/bsi/pw/aula03/ApplicationRest.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifg.luziania.bsi.pw.aula03;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class ApplicationRest extends Application {
-}
